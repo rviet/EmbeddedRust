3cb27dbb250788d8ea5be1666d2cd38cf4f65810
Rich Viet <richard.viet@gmail.com>
2021-04-15 23:09:55 +0000

    examples from emb rust book

---
+++ app3/src/main.rs
@@ -0,0 +1,54 @@
+#![no_std]
+#![no_main]
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use panic_halt as _; // panic handler
+use tm4c123x_hal as hal;
+use tm4c123x_hal::prelude::*;
+use tm4c123x_hal::serial::{NewlineMode, Serial};
+use tm4c123x_hal::sysctl;
+#[entry]
+fn main() -> ! {
+    let p = hal::Peripherals::take().unwrap();
+    let cp = hal::CorePeripherals::take().unwrap();
+    // Wrap up the SYSCTL struct into an object with a higher-layer API
+    let mut sc = p.SYSCTL.constrain();
+    // Pick our oscillation settings
+    sc.clock_setup.oscillator = sysctl::Oscillator::Main(
+        sysctl::CrystalFrequency::_16mhz,
+        sysctl::SystemClock::UsePll(sysctl::PllOutputFrequency::_80_00mhz),
+    );
+    // Configure the PLL with those settings
+    let clocks = sc.clock_setup.freeze();
+    // Wrap up the GPIO_PORTA struct into an object with a higher-layer API.
+    // Note it needs to borrow `sc.power_control` so it can power up the GPIO
+    // peripheral automatically.
+    let mut porta = p.GPIO_PORTA.split(&sc.power_control);
+
+    // Activate the UART.
+    let mut uart = Serial::uart0(
+        p.UART0,
+        // The transmit pin
+        porta
+            .pa1
+            .into_af_push_pull::<hal::gpio::AF1>(&mut porta.control),
+        // The receive pin
+        porta
+            .pa0
+            .into_af_push_pull::<hal::gpio::AF1>(&mut porta.control),
+        // No RTS or CTS required
+        (),
+        (),
+        // The baud rate
+        115200_u32.bps(),
+        // Output handling
+        NewlineMode::SwapLFtoCRLF,
+        // We need the clock rates to calculate the baud rate divisors
+        &clocks,
+        // We need this to power up the UART peripheral
+        &sc.power_control,
+    );
+    loop {
+        writeln!(uart, "Hello, World!\r\n").unwrap();
+    }
+}
