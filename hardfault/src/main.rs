93476315db64d5af531cd95c22ae7aae56268444
Rich Viet <richard.viet@gmail.com>
2021-04-16 02:21:22 +0000

    added hardfault

---
+++ hardfault/src/main.rs
@@ -0,0 +1,22 @@
+#![no_main]
+#![no_std]
+use core::fmt::Write;
+use core::ptr;
+use cortex_m_rt::{entry, exception, ExceptionFrame};
+use cortex_m_semihosting::hio;
+use panic_halt as _;
+#[entry]
+fn main() -> ! {
+    // read a nonexistent memory location
+    unsafe {
+        ptr::read_volatile(0x3FFF_FFFE as *const u32);
+    }
+    loop {}
+}
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    if let Ok(mut hstdout) = hio::hstdout() {
+        writeln!(hstdout, "{:#?}", ef).ok();
+    }
+    loop {}
+}
